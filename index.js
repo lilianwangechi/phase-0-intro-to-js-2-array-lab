865012fbbe68699a33a7e709afaaf308b67c0dc7
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-06 08:28:29 +0300

    Completed Assighnment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+
+
+function appendCat(name){
+    let newCats = cats.slice()
+    newCats.push(name)
+    return newCats;
+}
+
+function prependCat(name){
+    let newCats = cats.slice()
+    newCats.unshift(name)
+    return newCats;
+}
+
+function removeLastCat(){
+    let bigCats = cats.slice()
+    bigCats.pop()
+    return bigCats;
+}
+
+function removeFirstCat(){
+    let smallCats = cats.slice()
+    smallCats.shift()
+    return smallCats
+}
